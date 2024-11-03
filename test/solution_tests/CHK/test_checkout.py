924c92c6bcf30e9ffbb90af5e7ed5f7e2aee4b54
sbx_user1051 <sbx_user1051@169.254.92.5>
2024-11-03 13:44:08 +0000

    Sun Nov 03 13:44:08 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,7 +1,19 @@
-from solution_tests.CHK.checkout_solution import checkout
+from solutions.CHK.checkout_solution import checkout
 
 def test_illegal_checkout():
-    assert cehck
+    assert checkout("a") == -1
+    assert checkout("-") == -1
+    assert checkout("AxA") == -1
 
 def test_empty_checkout():
     assert checkout("") == 0
+
+
+def test_single_entry_checkout():
+    assert checkout("A") == 50
+    assert checkout("B") == 30
+
+
+def test_special_offers_checkout():
+    assert checkout("AAA") == 130
+    assert checkout("BBBB") == 90
