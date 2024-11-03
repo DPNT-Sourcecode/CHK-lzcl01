38f27e8e92dc2297eb36b358c777f9177359dc5a
sbx_user1051 <sbx_user1051@169.254.92.5>
2024-11-03 14:58:36 +0000

    Sun Nov 03 14:58:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,22 +1,58 @@
-prices = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-    'E': 40,
-    'F': 10
-}
+# prices = {
+#     'A': 50,
+#     'B': 30,
+#     'C': 20,
+#     'D': 15,
+#     'E': 40,
+#     'F': 10
+# }
 
-# Sorted by number of items descending
-special_offers = {
-    'A': [(5, 200), (3, 130)],
-    'B': [(2, 45)],
-}
+# # Sorted by number of items descending
+# special_offers = {
+#     'A': [(5, 200), (3, 130)],
+#     'B': [(2, 45)],
+# }
 
-free_items_offers = {
-    'E': (2, 'B'),
-    'F': (2, 'F')
-}
+# free_items_offers = {
+#     'E': (2, 'B'),
+#     'F': (2, 'F')
+# }
+
+table = """
+    | A    | 50    | 3A for 130, 5A for 200 |
+    | B    | 30    | 2B for 45              |
+    | C    | 20    |                        |
+    | D    | 15    |                        |
+    | E    | 40    | 2E get one B free      |
+    | F    | 10    | 2F get one F free      |
+    | G    | 20    |                        |
+    | H    | 10    | 5H for 45, 10H for 80  |
+    | I    | 35    |                        |
+    | J    | 60    |                        |
+    | K    | 80    | 2K for 150             |
+    | L    | 90    |                        |
+    | M    | 15    |                        |
+    | N    | 40    | 3N get one M free      |
+    | O    | 10    |                        |
+    | P    | 50    | 5P for 200             |
+    | Q    | 30    | 3Q for 80              |
+    | R    | 50    | 3R get one Q free      |
+    | S    | 30    |                        |
+    | T    | 20    |                        |
+    | U    | 40    | 3U get one U free      |
+    | V    | 50    | 2V for 90, 3V for 130  |
+    | W    | 20    |                        |
+    | X    | 90    |                        |
+    | Y    | 10    |                        |
+    | Z    | 50    |                        |
+"""
+
+prices = {}
+special_offers = {}
+free_items_offers = {}
+
+for line in table.split("\n"):
+    
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -53,4 +89,5 @@ def checkout(skus):
             total_price += num_bulks * bulk_price
 
     return total_price
+
 
