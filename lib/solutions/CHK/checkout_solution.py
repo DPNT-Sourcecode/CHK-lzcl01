567b1a132ca07fb14a0dca8a81f70f0e221e8c0c
sbx_user1051 <sbx_user1051@169.254.92.5>
2024-11-03 14:46:58 +0000

    Sun Nov 03 14:46:58 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -89,7 +89,7 @@ prices = {
 # Sorted by number of items descending
 special_offers = {
     'A': [(5, 200), (3, 130)],
-    'B': (2, 45),
+    'B': [(2, 45)],
 }
 
 free_items_offers = {
@@ -103,13 +103,15 @@ def checkout(skus):
     # Count items in basket    
     item_counts = {}
     for sku in skus:
+        if sku not in prices:
+            return -1
         item_counts[sku] = item_counts.get(sku, 0) + 1
 
     # Count free items
     free_item_counts = {}
     for sku, offer in free_items_offers.items():
         required_amount, free_item = offer
-        num_frees = item_counts[sku] // required_amount
+        num_frees = item_counts.get(sku, 0) // required_amount
         free_item_counts[free_item] = free_item_counts.get(free_item, 0) + num_frees
 
     total_price = 0
@@ -117,8 +119,8 @@ def checkout(skus):
     for sku, count in item_counts.items():
         count -= free_item_counts.get(sku, 0)
 
-        sp_offers = special_offers[sku]
-        sp_offers.append(1, prices[sku])
+        sp_offers = special_offers.get(sku, [])
+        sp_offers.append((1, prices[sku]))
 
         for bulk_amount, bulk_price in sp_offers:
             num_bulks = count // bulk_amount
@@ -126,11 +128,3 @@ def checkout(skus):
             total_price += num_bulks * bulk_price
 
     return total_price
-
-
-
-
-
-
-
-
