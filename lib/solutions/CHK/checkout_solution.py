211257fe966b974cdf5bf2b3424ef963409ec062
sbx_user1051 <sbx_user1051@169.254.92.5>
2024-11-03 13:22:21 +0000

    Sun Nov 03 13:22:21 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,17 @@
 class SpecialOffer:
-    def __init__(item_count, special_price):
+    def __init__(self, item_count, special_price):
+        self.item_count = item_count
+        self.special_price = special_price
+
+
+special_offers = {
+    'A': SpecialOffer(3, 130),
+    'B': SpecialOffer(2, 45)
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
 
